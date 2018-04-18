b4795278241459562770aecbc981a9aeac5f7a39
obscene3190 <starkiller44@yandex.com>
2018-04-18 19:02:20 +0300

    added doxygen.conf

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.org/obscene3190/lab06.svg?branch=master)](https://travis-ci.org/obscene3190/lab06)
+[![Build Status](https://travis-ci.org/obscene3190/lab07.svg?branch=master)](https://travis-ci.org/obscene3190/lab07)
 ## Laboratory work III
 
 Данная лабораторная работа посвещена изучению систем контроля версий на примере **Git**.
--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,5 @@
 #include <fstream>
 #include <iostream>
 
-void print(const std::string& text, std::ostream& out = std::cout);
-void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ostream& out = std::cout); //выводит файлы в стандартный поток
+void print(const std::string& text, std::ofstream& out); //выводит файлы в файл
