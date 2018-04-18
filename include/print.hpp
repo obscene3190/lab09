c4d2ac59314c3d4cf058e4bbc94abe755d550c9f
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-04-18 19:12:14 +0300

    Update print.hpp

--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,5 @@
 #include <fstream>
 #include <iostream>
 
-void print(const std::string& text, std::ostream& out = std::cout); //выводит файлы в стандартный поток
-void print(const std::string& text, std::ofstream& out); //выводит файлы в файл
+void print(const std::string& text, std::ostream& out = std::cout); //выводит текст в стандартный поток
+void print(const std::string& text, std::ofstream& out); //выводит текст в файл
